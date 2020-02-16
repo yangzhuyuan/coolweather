93dcaa498de67e8c27e1fb49bb6ef8abf7b950d3
Yangzhuyuan <Yangzhuyuan@gmail.com>
2020-02-16 15:35:24 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -2,7 +2,14 @@ package com.coolweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+
+import com.coolweather.android.gson.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences (this);
+        if (prefs.getString ("weather",null) != null ) {
+            Intent intent = new Intent (this, WeatherActivity.class);
+            startActivity (intent);
+            finish ();
+        }
     }
 }
