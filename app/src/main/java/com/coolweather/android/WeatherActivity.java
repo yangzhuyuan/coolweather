0c2d182884d436a0859552dad4d84687d363ed08
Yangzhuyuan <Yangzhuyuan@gmail.com>
2020-02-17 14:34:05 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 import java.io.IOException;
@@ -146,8 +148,9 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
-        loadBingPic();
+        loadBingPic ();
     }
+
     /**
      * 加载必应每日一图
      */
@@ -167,19 +170,21 @@ public class WeatherActivity extends AppCompatActivity {
                     }
                 });
             }
+
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace ();
             }
         });
     }
+
     /**
      * 处理并展示Weather实体类中的数据
      */
     private void showWeatherInfo(Weather weather) {
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split (" ")[1];
-        String degree = weather.now.temperature + "℃" ;
+        String degree = weather.now.temperature + "℃";
         String weatherInfo = weather.now.more.info;
         titleCity.setText (cityName);
         titleUpdateTime.setText (updateTime);
@@ -209,8 +214,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText (carWash);
         sportText.setText (sport);
         weatherLayout.setVisibility (View.VISIBLE);
-        }
+        Intent intent = new Intent (this, AutoUpdateService.class);
+        startService (intent);
 
-    private class SwipeRefewshLayout {
     }
 }
+
